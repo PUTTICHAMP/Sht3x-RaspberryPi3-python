14c8b2bba4accfc05b7f753411dfe90f20be9998
PUTTICHAMP <40465078+PUTTICHAMP@users.noreply.github.com>
2018-07-13 12:24:49 +0700

    Add files via upload

---
+++ SHT10.ino
@@ -0,0 +1,34 @@
+
+
+#include "SHT11.h"
+
+esl::SHT11 sht11( 0 /*data*/, 2 /*clk*/ );
+
+void setup() {
+  Serial.begin( 115200 );
+}
+
+char sbuf[30];
+
+void loop() {
+  float val;
+  if ( sht11.readTemperature(val) ) {
+    int16_t t = (int16_t)(100*val);
+    sprintf( sbuf, "%d.%02d deg.C, ", t/100, abs(t%100) );
+    Serial.print( sbuf );
+  } else {
+    Serial.println( "Sensor Reading Failed! " );
+  }
+  delay(50);
+  if ( sht11.readHumidity(val) ) {
+    int16_t t = (int16_t)(100*val);
+    sprintf( sbuf, "%d.%02d%c RH", t/100, t%100, '%' );
+    Serial.println( sbuf );
+  } else {
+    Serial.println( "Sensor Reading Failed! " );
+  }
+  delay(5000);
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+
